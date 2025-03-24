9bc960bf5446974f9bd77cf846bd434d4b49538f
minhkhoa22 <minhkhoa090201@gmail.com>
2025-03-24 22:06:12 +0700

    form create user

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,5 +1,39 @@
 package vn.hoidanit.laptopshop.controller;
 
+import vn.hoidanit.laptopshop.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.ui.Model;
+import vn.hoidanit.laptopshop.service.UserService;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.handHello();
+        model.addAttribute("tast", test);
+        return "hello";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User minhkhoa) {
+        System.out.println("run" + minhkhoa);
+        return "hello";
+    }
 
 }
